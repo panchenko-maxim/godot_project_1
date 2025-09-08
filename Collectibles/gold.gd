1b7bbe210ea0c840d4df3298ee1e756c62237d8d
root221 <panchenko.maxim87@gmail.com>
2025-09-08 16:50:42 +0300

    les_7 - 10:38 create jump coins

--- Collectibles/gold.gd
+++ Collectibles/gold.gd
@@ -4,5 +4,8 @@ extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
+		var tween = get_tree().create_tween()
+		tween.tween_property(self, "position", position - Vector2(0, 25), 0.3)
+		tween.tween_callback(queue_free)
 		body.gold += 1
-		queue_free()
+		
