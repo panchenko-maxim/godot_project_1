015855c13fb39ba5486686ee96533141a37a7435
root221 <panchenko.maxim87@gmail.com>
2025-09-09 16:54:03 +0300

    end 7 les

---
+++ collectibles.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+
+var gold = preload("res://Collectibles/gold.tscn")
+
+func _on_timer_timeout() -> void:
+	var goldTemp = gold.instantiate()
+	var rng = RandomNumberGenerator.new()
+	var randint = randi_range(50, 500)
+	goldTemp.position = Vector2(randint, 615)
+	add_child(goldTemp)
